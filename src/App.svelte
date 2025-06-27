17311984c060ce94f59bcd58a1eadfd080fa61e0
Algolbarth <paul.poirier@live.fr>
2025-06-27 21:56:56 +0200

    feat: add canvas element

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { onDestroy, onMount } from "svelte";
+  import { color } from "./lib/color";
+
+  let canvas: HTMLCanvasElement;
+  let ctx: CanvasRenderingContext2D;
+  let fps: number = 60;
+
+  function step() {
+    ctx.fillStyle = color.black;
+    ctx.fillRect(0, 0, 1920, 1080);
+  }
+
+  onMount(() => {
+    canvas.width = 1920;
+    canvas.height = 1080;
+
+    ctx = canvas.getContext("2d")!;
+
+    setInterval(step, 1000 / fps);
+  });
+
+  onDestroy(() => {});
+</script>
+
+<canvas bind:this={canvas}></canvas>
